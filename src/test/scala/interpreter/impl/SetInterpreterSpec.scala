d1b938604c8bedf6cf88bc79aafa668289391267
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-02-06 15:29:23 +0100

    tests for set interpreter

--- src/test/scala/interpreter/impl/SetInterpreterSpec.scala
+++ src/test/scala/interpreter/impl/SetInterpreterSpec.scala
@@ -1,6 +1,33 @@
 package interpreter.impl
 
-class SetInterpreterSpec {
+import interpreter.{BlockIp, Context}
+import models.{Block, IntValue, LocalVariable}
 
-  // TODO implement
+class SetInterpreterSpec extends BaseInterpreterSpec {
+
+  "SetInterpreter" should "set local variables" in new Env {
+    val c = run(ctx)("<SET A 1>")
+    c.get(LocalVariable("A")) should be(IntValue(1))
+  }
+
+  it should "set the local variables in closest scope" in new Env {
+    ctx.ip.isScope should be(true)
+    val c = new Context(BlockIp(Block(Seq())), Some(ctx))
+    c.ip.isScope should be(false)
+
+    run(c)("<SET A 1>")
+    c.get(LocalVariable("A")) should be(IntValue(1))
+  }
+
+  it should "fail with too many parameters" in new Env {
+    intercept[IllegalArgumentException] {
+      run(ctx)("<SET A 1 2>")
+    }
+  }
+
+  it should "fail with too few parameters" in new Env {
+    intercept[IllegalArgumentException] {
+      run(ctx)("<SET A>")
+    }
+  }
 }
