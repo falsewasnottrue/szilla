27948cf072dcaeec8cf1f3e67da2afd8f28819f9
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-02-08 12:03:32 +0100

    parser for syntax tokens

--- src/main/scala/models/Syntax.scala
+++ src/main/scala/models/Syntax.scala
@@ -1,20 +1,89 @@
 package models
 
-
-case class Syntax(verbId: String, verb: String, obj1: Option[ObjectToken] = None, prep: Option[String] = None, obj2: Option[ObjectToken] = None) {
-
-}
+case class Syntax(verbId: String, verb: String,
+                  preAction: Option[String] = None,
+                  obj1: Option[ObjectToken] = None,
+                  prep: Option[String] = None,
+                  obj2: Option[ObjectToken] = None)
 
 object Syntax {
-  val SYNTAX = """<SYNTAX (\S+) (OBJECT)? = (\S+)>""".r
-  // val SYNTAX = """<SYNTAX (.+) (OBJECT (\(.*\))?)? (.+)? (OBJECT (\(.*\))?)? = (\S+)>""".r
+  val SYNTAX = """<SYNTAX (.+)=(.+)>""".r
 
-  def unapply(str: String): Option[Syntax] = str match {
-    case SYNTAX(verb, null, verbId) => Some(Syntax(verbId, verb))
-    case SYNTAX(verb, obj1, verbId) =>
-      Some(Syntax(verbId, verb, ObjectToken.unapply(obj1)))
+  def unapply(str: String): Option[Syntax] = {
+    str match {
+      case SYNTAX(formPart, idPart) => {
+        val (verbId, preAction) = idPart.trim.split(" ").toSeq.map(_.trim) match {
+          case Seq(v) => (v, None)
+          case Seq(v,p) => (v, Some(p))
+          case _ => throw new IllegalArgumentException(s"illegal idPart $idPart")
+        }
 
-    case _ => None
+        val ps =  formPart.split(" ").toSeq.map(_.trim).foldLeft((Seq[String](), 0)) {
+          case ((Nil, 0), curr) => (Seq(curr), 0)
+          //  0 = Start
+          case ((Seq(v), 0), curr) =>
+              if (curr != "OBJECT") {
+                (Seq(v + " " + curr), 0)
+              } else {
+                (Seq(v, curr), 1)
+              }
+          //  1 = Object gelesen
+          case ((Seq(v,o1), 1), curr) =>
+            if (curr.startsWith("(") && curr.endsWith(")")) {
+              (Seq(v, o1 + " " + curr), 3)
+            } else if (curr.startsWith("(")) {
+              (Seq(v, o1 + " " + curr), 2)
+            } else if (curr == "OBJECT") {
+              (Seq(v, o1, "", curr), 4)
+            } else {
+              (Seq(v, o1, curr), 3)
+            }
+          //  2 = ( gelesen
+          case ((Seq(v,o1), 2), curr) =>
+            if (curr.endsWith(")")) {
+              (Seq(v, o1 + " " + curr), 3)
+            } else {
+              (Seq(v, o1 + " " + curr), 2)
+            }
+          //  3 = ) gelesen oder keine (
+          case ((Seq(v,o1), 3), curr) =>
+            if (curr == "OBJECT") {
+              (Seq(v,o1,"",curr), 4)
+            } else {
+              (Seq(v,o1,curr), 3)
+            }
+          case ((Seq(v,o1,p), 3), curr) =>
+            if (curr == "OBJECT") {
+              (Seq(v,o1,p,curr), 4)
+            } else {
+              (Seq(v,o1,p + " "+ curr), 3)
+            }
+          //  4 = Object gelesen
+          case ((Seq(v,o1,p,o2), 4), curr) =>
+            if (curr.startsWith("(")) {
+              (Seq(v,o1,p,o2 + " " + curr), 5)
+            } else {
+              throw new IllegalStateException(s"trailing syntax element $curr")
+            }
+          //  5 = ( gelesen
+          case ((Seq(v,o1,p,o2), 5), curr) =>
+            (Seq(v,o1,p,o2 + " " + curr), 5)
+        }._1
+
+        ps match {
+          case Seq(verb) => Some(Syntax(verbId, verb, preAction))
+          case Seq(verb, obj1) => Some(Syntax(verbId, verb, preAction, obj1 = ObjectToken.unapply(obj1)))
+          case Seq(verb, obj1, "", obj2) =>
+            Some(Syntax(verbId, verb, preAction,
+              obj1 = ObjectToken.unapply(obj1), prep = None, obj2 = ObjectToken.unapply(obj2)))
+          case Seq(verb, obj1, prep, obj2) =>
+            Some(Syntax(verbId, verb, preAction,
+              obj1 = ObjectToken.unapply(obj1), prep = Some(prep), obj2 = ObjectToken.unapply(obj2)))
+          case _ => throw new IllegalArgumentException(s"cannot parse syntax $ps")
+        }
+      }
+      case _ => throw new IllegalArgumentException(s"not a syntax: $str")
+    }
   }
 
 }
--- src/test/scala/parsers/SyntaxParserSpec.scala
+++ src/test/scala/parsers/SyntaxParserSpec.scala
@@ -9,56 +9,119 @@ class SyntaxParserSpec extends FlatSpec with Matchers {
     val Some(syntax) = Syntax.unapply("<SYNTAX EAT OBJECT = V-EAT>")
 
     syntax.verbId should be("V-EAT")
+    syntax.preAction should be(None)
+
     syntax.verb should be("EAT")
     syntax.obj1 should be(Some(ObjectToken(Nil)))
     syntax.prep should be(None)
     syntax.obj2 should be(None)
   }
 
+
   it should "parse <SYNTAX LOOK THROUGH OBJECT = V-LOOK-INSIDE>" in {
     val Some(syntax) = Syntax.unapply("<SYNTAX LOOK THROUGH OBJECT = V-LOOK-INSIDE>")
 
     syntax.verbId should be("V-LOOK-INSIDE")
+    syntax.preAction should be(None)
+
     syntax.verb should be("LOOK THROUGH")
     syntax.obj1 should be(Some(ObjectToken(Nil)))
     syntax.prep should be(None)
     syntax.obj2 should be(None)
   }
 
-  it should "parse <SYNTAX LOOK UNDER OBJECT = V-LOOK-UNDER>" in {
-    val Some(syntax) = Syntax.unapply("<SYNTAX LOOK UNDER OBJECT = V-LOOK-UNDER>")
+ it should "parse <SYNTAX LOOK UNDER OBJECT = V-LOOK-UNDER>" in {
+   val Some(syntax) = Syntax.unapply("<SYNTAX LOOK UNDER OBJECT = V-LOOK-UNDER>")
 
-    syntax.verbId should be("V-LOOK-UNDER")
-    syntax.verb should be("LOOK UNDER")
+   syntax.verbId should be("V-LOOK-UNDER")
+   syntax.preAction should be(None)
+
+   syntax.verb should be("LOOK UNDER")
+   syntax.obj1 should be(Some(ObjectToken(Nil)))
+   syntax.prep should be(None)
+   syntax.obj2 should be(None)
+ }
+
+ it should "parse <SYNTAX INVENTORY = V-INVENTORY>" in {
+   val Some(syntax) = Syntax.unapply("<SYNTAX INVENTORY = V-INVENTORY>")
+
+   syntax.verbId should be("V-INVENTORY")
+   syntax.preAction should be(None)
+
+   syntax.verb should be("INVENTORY")
+   syntax.obj1 should be(None)
+   syntax.prep should be(None)
+   syntax.obj2 should be(None)
+ }
+
+  it should "parse <SYNTAX GIVE OBJECT TO OBJECT = V-GIVE>" in {
+    val Some(syntax) = Syntax.unapply("<SYNTAX GIVE OBJECT TO OBJECT = V-GIVE>")
+
+    syntax.verbId should be("V-GIVE")
+    syntax.preAction should be(None)
+
+    syntax.verb should be("GIVE")
+    syntax.obj1 should be(Some(ObjectToken(Nil)))
+    syntax.prep should be(Some("TO"))
+    syntax.obj2 should be(Some(ObjectToken(Nil)))
+  }
+
+  it should "parse <SYNTAX TAKE OBJECT (FIND TAKEBIT) = V-TAKE>" in {
+    val Some(syntax) = Syntax.unapply("<SYNTAX TAKE OBJECT (FIND TAKEBIT) = V-TAKE>")
+
+    syntax.verbId should be("V-TAKE")
+    syntax.preAction should be(None)
+
+    syntax.verb should be("TAKE")
+    syntax.obj1 should be(Some(ObjectToken(Seq(FIND("TAKEBIT")))))
+    syntax.prep should be(None)
+    syntax.obj2 should be(None)
+  }
+
+  it should "parse <SYNTAX ATTACK OBJECT WITH OBJECT (FIND WEAPONBIT) = V-ATTACK>" in {
+    val Some(syntax) = Syntax.unapply("<SYNTAX ATTACK OBJECT WITH OBJECT (FIND WEAPONBIT) = V-ATTACK>")
+
+    syntax.verbId should be("V-ATTACK")
+    syntax.preAction should be(None)
+    syntax.verb should be("ATTACK")
+    syntax.obj1 should be(Some(ObjectToken(Nil)))
+    syntax.prep should be(Some("WITH"))
+    syntax.obj2 should be(Some(ObjectToken(Seq(FIND("WEAPONBIT")))))
+  }
+
+  it should "parse <SYNTAX FIRE AT OBJECT = V-SHOOT PRE-FIRE>" in {
+    val Some(syntax) = Syntax.unapply("<SYNTAX FIRE AT OBJECT = V-SHOOT PRE-FIRE>")
+
+    syntax.verbId should be("V-SHOOT")
+    syntax.preAction should be(Some("PRE-FIRE"))
+
+    syntax.verb should be("FIRE AT")
     syntax.obj1 should be(Some(ObjectToken(Nil)))
     syntax.prep should be(None)
     syntax.obj2 should be(None)
   }
 
-  it should "parse <SYNTAX INVENTORY = V-INVENTORY>" in {
-    val Some(syntax) = Syntax.unapply("<SYNTAX INVENTORY = V-INVENTORY>")
+  it should "parse <SYNTAX GIVE OBJECT (HAVE) TO OBJECT (ON-GROUND IN-ROOM) = V-GIVE>" in {
+    val Some(syntax) = Syntax.unapply("<SYNTAX GIVE OBJECT (HAVE) TO OBJECT (ON-GROUND IN-ROOM) = V-GIVE>")
 
-    syntax.verbId should be("V-INVENTORY")
-    syntax.verb should be("INVENTORY")
-    syntax.obj1 should be(None)
+    syntax.verbId should be("V-GIVE")
+    syntax.preAction should be(None)
+
+    syntax.verb should be("GIVE")
+    syntax.obj1 should be(Some(ObjectToken(Seq(HAVE))))
+    syntax.prep should be(Some("TO"))
+    syntax.obj2 should be(Some(ObjectToken(Seq(ONGROUND, INROOM))))
+  }
+
+  it should "parse <SYNTAX EXAMINE OBJECT (MANY) = V-EXAMINE>" in {
+    val Some(syntax) = Syntax.unapply("<SYNTAX EXAMINE OBJECT (MANY) = V-EXAMINE>")
+
+    syntax.verbId should be("V-EXAMINE")
+    syntax.preAction should be(None)
+
+    syntax.verb should be("EXAMINE")
+    syntax.obj1 should be(Some(ObjectToken(Seq(MANY))))
     syntax.prep should be(None)
     syntax.obj2 should be(None)
   }
-
-  // <SYNTAX GIVE OBJECT TO OBJECT = V-GIVE>
-
-  /*
-  <SYNTAX GET OBJECT = V-TAKE>
-  <SYNTAX GET IN OBJECT = V-ENTER>
-  <SYNTAX GET ON OBJECT = V-ENTER>
-  <SYNTAX GET OFF OBJECT = V-EXIT>
-  <SYNTAX GET OBJECT WITH OBJECT = V-TAKE-WITH>
-   */
-  // <SYNTAX TAKE OBJECT (FIND TAKEBIT) = V-TAKE>
-  // <SYNTAX ATTACK OBJECT WITH OBJECT (FIND WEAPONBIT)= V-ATTACK>
-  // <SYNTAX FIRE AT OBJECT = V-SHOOT PRE-FIRE>
-
-  // Tokens
-  // <SYNTAX GIVE OBJECT (HAVE) TO OBJECT (ON-GROUND IN-ROOM) = V-GIVE>
-  // <SYNTAX EXAMINE OBJECT (MANY) = V-EXAMINE>
 }
